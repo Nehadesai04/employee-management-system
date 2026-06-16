622294a118ccc381279ac3ff6875b290e53c11c7
omdesai123 <omddesai0908@gmail.com>
2026-06-16 12:33:16 +0530

    Frontend production api url

--- Frontend/employee-management-tracking/src/services/api.js
+++ Frontend/employee-management-tracking/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://employee-management-system-lxzp.onrender.com/api",
 });
 
 // REQUEST: attach token
